b4ae03340a425643699766bd37edaba6fe6a7e3e
pokro <pokro@GURYNYCH>
2023-09-18 14:58:59 +0300

    Добавьте файлы проекта.

---
+++ RemoteControlWPFClient/MVVM/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RemoteControlWPFClient.MVVM.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(propertyName, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ RemoteControlWPFClient/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteControlWPFClient.MVVM.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+    }
+}
